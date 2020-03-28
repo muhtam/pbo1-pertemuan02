12710f07d49055fe1c6083b9388583fdb797fc37
muhtam <gantengnajib@gmail.com>
2020-03-28 05:49:46 -0700

    Memanggil method dari banyak Class di package lain

---
+++ model/mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+  public static void tampilanInfo(){
+    System.out.println();
+    System.out.println("==Data Mata Kuliah =================");
+    System.out.println("Kode : TIF3402");
+    System.out.println("Nama : Muhtam Nadjib");
+    System.out.println("==============================");
+ }
+}
